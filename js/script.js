d1c075d5f767b3c27156c4a1ec11b008a9248c09
Omolara5861 <debbiegterra@gmail.com>
2024-03-25 09:28:35 +0100

    refactor: :sparkles: Add testimonial

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
